27b827c4d02f97f5da1f7fb258aac39f7b86ef1f
Carl Hammarling <carlhammarling@gmail.com>
2023-11-23 16:51:25 +0000

    Emptying or decreasing cart-state functioning from Cart

--- src/routes/cart/index@navbar.tsx
+++ src/routes/cart/index@navbar.tsx
@@ -1,7 +1,28 @@
-import { component$, useContext } from '@builder.io/qwik';
-import { CartContext } from '~/contexts/cartContext';
+import { component$, useContext, $ } from "@builder.io/qwik";
+import { CartContext } from "~/contexts/cartContext";
 
 export default component$(() => {
-    const cartData = useContext(CartContext)
-  return <div>{cartData.productIds}</div>
+  const cartData = useContext(CartContext);
+
+  const emptyCart = $(() => {
+    cartData.productIds = [];
+    cartData.count = 0;
+  });
+
+  const decreceCart = $(() => {
+    if(cartData.count > 0) {
+      cartData.productIds.pop();
+      cartData.count--;
+    }
+  });
+
+  return (
+    <div>
+      {cartData.productIds && cartData.productIds.map((item, index) => (
+        <p key={index}>{item}</p>
+      ))}
+      <button onClick$={emptyCart}>Empty Cart</button>
+      <button onClick$={decreceCart}>Decrece cart</button>
+    </div>
+  );
 });
